0d608707d2c5028fc13a9a53036b01ce126abfb0
LeoKGX <leokaltengomez@gmail.com>
2022-08-24 20:49:22 -0300

    post update

--- src/main/java/com/miapp/portfolio/controller/CertificadoController.java
+++ src/main/java/com/miapp/portfolio/controller/CertificadoController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class CertificadoController {
     }
     
     
-    @PutMapping ("/crear/cert")
+    @PostMapping ("/crear/cert")
     public ResponseEntity<?> crearEdu(@RequestBody Certificado certificado){
         certificadoRepo.save(certificado);
         return ResponseEntity.ok().body("");
--- src/main/java/com/miapp/portfolio/controller/PersonaController.java
+++ src/main/java/com/miapp/portfolio/controller/PersonaController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class PersonaController {
     @Autowired
     IPersonaRepo personaRepo;
     
-    @PutMapping ("/crear/persona")
+    @PostMapping ("/crear/persona")
     public ResponseEntity<Persona> crearPersona(@RequestBody @Valid Persona persona){
         personaRepo.save(persona);
         URI personaUri = URI.create("/persona/" + persona.getId());
