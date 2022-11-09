0580eaf6719dcdbe89afffb2b033790cefccfc23
LeoKGX <56890052+LeoKGX@users.noreply.github.com>
2022-11-08 22:01:49 -0300

    Update app sec config

--- src/main/java/com/miapp/portfolio/ApplicationSecurityConfig.java
+++ src/main/java/com/miapp/portfolio/ApplicationSecurityConfig.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,7 +52,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
      
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors(withDefaults());
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     
