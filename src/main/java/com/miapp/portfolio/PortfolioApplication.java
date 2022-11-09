5a0a82c3cb1bee60a42da2bdc49fd1c32b79db39
LeoKGX <56890052+LeoKGX@users.noreply.github.com>
2022-11-08 21:50:39 -0300

    Update PortfolioApplication.java

--- src/main/java/com/miapp/portfolio/PortfolioApplication.java
+++ src/main/java/com/miapp/portfolio/PortfolioApplication.java
@@ -2,9 +2,6 @@ package com.miapp.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -12,13 +9,5 @@ public class PortfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-        @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/login").allowedOrigins("*");
-			}
-		};
-	}
+
 }
