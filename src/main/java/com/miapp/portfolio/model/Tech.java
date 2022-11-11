cd94ea7a1070c9eff3a045367ad6d20ef10c7528
LeoKGX <56890052+LeoKGX@users.noreply.github.com>
2022-11-11 19:00:54 -0300

    update tech

--- src/main/java/com/miapp/portfolio/model/Tech.java
+++ src/main/java/com/miapp/portfolio/model/Tech.java
@@ -1,14 +1,10 @@
 
 package com.miapp.portfolio.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,19 +14,6 @@ public class Tech {
     private Long id;
     
     private String name;
-    
-    @ManyToMany(mappedBy = "techs")
-    @JsonIgnore
-    private List<Proyecto> proyecto = new ArrayList<>();
-
-    public List<Proyecto> getProyecto() {
-        return proyecto;
-    }
-
-    public void setProyecto(List<Proyecto> proyecto) {
-        this.proyecto = proyecto;
-    }
-    
 
     public Long getId() {
         return id;
